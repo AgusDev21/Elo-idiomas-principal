a69836c09048fc86f0bc53747c768d10481a247c
Magtured <magturedagus@users.noreply.github.com>
2021-03-09 15:36:31 -0600

    testimoniales1
    primera parte de la creacion y animacion de testimoniales

--- js/main.js
+++ js/main.js
@@ -18,4 +18,21 @@ contador.forEach(counter => {
 
   updateCount();
 });
+//testimoniales
+var swiper = new Swiper('.swiper-container', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
 
